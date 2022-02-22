983c3491adbe3e7c652449009e71bb923b69ea58
mgamamunbd <mgamamunbd@gmail.com>
2022-02-22 09:51:21 +0400

    photo added

--- api-intro.js
+++ api-intro.js
@@ -42,3 +42,38 @@ function loadData() {
   loadUsers();
   loadPosts();
 }
+
+//album section
+
+function loadAlbums() {
+  fetch("https://jsonplaceholder.typicode.com/albums")
+    .then((response) => response.json())
+    .then((data) => displayAlbums(data));
+}
+// loadAlbums();
+
+function displayAlbums(albums) {
+  const albumContainer = document.getElementById("albums");
+  for (const album of albums) {
+    const p = document.createElement("p");
+    p.style.textAlign = "center";
+    p.innerHTML = album.title;
+    albumContainer.appendChild(p);
+  }
+}
+
+//load photos
+function loadPhotos() {
+  fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((response) => response.json())
+    .then((data) => displayPhotos(data));
+}
+loadPhotos();
+function displayPhotos(photos) {
+  const photoContainer = document.getElementById("photos");
+  for (const photo of photos) {
+    const img = document.createElement("img");
+    img.src = photo.thumbnailUrl;
+    photoContainer.appendChild(img);
+  }
+}
